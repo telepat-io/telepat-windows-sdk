ba40a2a97ff3dddf153b29ec54e562ffeb01194c
ddorin <dorin@appscend.com>
2016-03-28 16:30:35 +0300

    updated app to login to new markiza test server

--- TelepatProject/MainPage.xaml.cs
+++ TelepatProject/MainPage.xaml.cs
@@ -17,6 +17,7 @@ using Windows.ApplicationModel;
 using TelepatProject.Utils;
 using TelepatSDK.Models;
 using TelepatProject.Data;
+using Windows.UI.Popups;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -33,84 +34,77 @@ namespace TelepatProject
         public MainPage()
         {
             this.InitializeComponent();
+
+            Telepat.GetInstance().LoginUserSuccess += OnLoginUserSuccess;
+            Telepat.GetInstance().LoginUserFailure += OnLoginUserFailure;
+
+            Telepat.GetInstance().LogoutSuccess += OnLogoutSuccess;
+            Telepat.GetInstance().LogoutFailure += OnLogoutFailure;
         }
 
-        private async void Grid_Loaded(object sender, RoutedEventArgs e)
-        {
-            Telepat.GetInstance().DeviceRegistered += delegate {
+        private void OnLoginUserSuccess(object sender, string args) {
+            TaskHelper.RunOnUiThread(this.Dispatcher, async () =>
+            {
+                var dialog = new MessageDialog("User " + args + " logged in!");
+                await dialog.ShowAsync();
+            });
+            // subscribe to polls
+            //pollsChannel = await Telepat
+            //                     .GetInstance()
+            //                     .Subscribe<polls>(Telepat
+            //                                       .GetInstance()
+            //                                       .GetContexts()
+            //                                       .Get("dda11deb-a259-43d8-b5ee-cf1010dc8a00"),
+            //                                       "polls",
+            //                                       new ChannelEventListener()
+            //                                       {
+            //                                           SubscribeComplete = OnPollsSubscribeComplete,
 
-                TaskHelper.RunOnUiThread(this.Dispatcher, () => {
+            //                                           ObjectCreateSuccess = delegate
+            //                                           {
+            //                                               int x = 0;
+            //                                           },
+            //                                           ObjectAdded = delegate
+            //                                           {
+            //                                               int x = 0;
+            //                                           },
+            //                                           ObjectModified = delegate
+            //                                           {
+            //                                               int x = 0;
+            //                                           },
+            //                                           ObjectRemoved = delegate
+            //                                           {
+            //                                               int x = 0;
+            //                                           },
+            //                                           Error = delegate
+            //                                           {
+            //                                               int x = 0;
+            //                                           }
+            //                                       });
+        }
 
-                    button.IsEnabled = true;
-                    button1.IsEnabled = true;
-                    button3.IsEnabled = true;
-                    button4.IsEnabled = true;
-                });
-            };
+        private void OnLoginUserFailure(object sender, string args) {
+            TaskHelper.RunOnUiThread(this.Dispatcher, async () =>
+            {
+                var dialog = new MessageDialog("Unable to login: " + args);
+                await dialog.ShowAsync();
+            });
+        }
 
-            Telepat.GetInstance().LoginUserSuccess += async delegate(object s, string arg) {
+        private void OnLogoutSuccess(object sender, object args) {
+            TaskHelper.RunOnUiThread(this.Dispatcher, async () =>
+            {
+                var dialog = new MessageDialog("dorin@appscend.com logged out!");
+                await dialog.ShowAsync();
+            });
+        }
 
-                pollsChannel = await Telepat
-                                     .GetInstance()
-                                     .Subscribe<polls>(Telepat
-                                                       .GetInstance()
-                                                       .GetContexts()
-                                                       .Get("dda11deb-a259-43d8-b5ee-cf1010dc8a00"),
-                                                       "polls",
-                                                       new ChannelEventListener()
-                                                       {
-                                                           SubscribeComplete = OnPollsSubscribeComplete,
-
-                                                           ObjectCreateSuccess = delegate
-                                                           {
-                                                               int x = 0;
-                                                           },
-                                                           ObjectAdded = delegate
-                                                           {
-                                                               int x = 0;
-                                                           },
-                                                           ObjectModified = delegate
-                                                           {
-                                                               int x = 0;
-                                                           },
-                                                           ObjectRemoved = delegate
-                                                           {
-                                                               int x = 0;
-                                                           },
-                                                           Error = delegate
-                                                           {
-                                                               int x = 0;
-                                                           }
-                                                       });
-            };
-
-            Telepat.GetInstance().LoginUserFailure += delegate (object s, string message) {
-                int x = 0;
-            };
-
-            Telepat.GetInstance().LogoutSuccess += delegate {
-                int x = 0;
-            };
-
-            Telepat.GetInstance().LogoutFailure += delegate {
-                int x = 0;
-            };
-
-            Telepat.GetInstance().RequestPasswordResetFailure += delegate {
-                int x = 0;
-            };
-
-            Telepat.GetInstance().RequestPasswordResetSuccess += delegate {
-                int x = 0;
-            };
-
-            await Telepat.GetInstance().Initialize("http://devel-api.cloudapp.net:3000/",
-                                                   "e10adc3949ba59abbe56e057f20f883e",
-                                                   "0a686e52-36ef-4c45-8ab4-b228b57af86a",
-                                                   true,
-                                                   "http://devel-workers.cloudapp.net");
-
-            Telepat.GetInstance().LoginWithUsername("dorin@veeplay.com", "parola");
+        private void OnLogoutFailure(object sender, object args) {
+            TaskHelper.RunOnUiThread(this.Dispatcher, async () =>
+            {
+                var dialog = new MessageDialog("Unable to logout: " + args);
+                await dialog.ShowAsync();
+            });
         }
 
         async void OnPollsSubscribeComplete(object sender, EventArgs args)
@@ -174,26 +168,63 @@ namespace TelepatProject
                             .GetChannelObjects<answers>((sender as Channel).GetSubscriptionIdentifier());
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+
+        private async void Grid_Loaded(object sender, RoutedEventArgs e)
         {
-            
+            Telepat.GetInstance().DeviceRegistered += OnDeviceRegistered;
+
+            // init telepat
+            await Telepat.GetInstance().Initialize("http://devel-api.cloudapp.net:3000/",
+                                                   "e10adc3949ba59abbe56e057f20f883e",
+                                                   "0a686e52-36ef-4c45-8ab4-b228b57af86a",
+                                                   true,
+                                                   "http://devel-workers.cloudapp.net");
         }
 
-        private void button1_Click(object sender, RoutedEventArgs e)
+        private void OnDeviceRegistered(object sender, object args) {
+            TaskHelper.RunOnUiThread(this.Dispatcher, () => {
+
+                // activate UI buttons
+                foreach (var item in buttonContainer.Children)
+                {
+                    if (item is Button) (item as Button).IsEnabled = true;
+                }
+            });
+        }
+
+        private void register_Click(object sender, RoutedEventArgs e)
+        {
+            Telepat.GetInstance().CreateUserSuccess += delegate {
+                TaskHelper.RunOnUiThread(this.Dispatcher, () =>
+                {
+                    register.IsEnabled = false;
+                    var dialog = new MessageDialog("User created succesfully!");
+                    var task = dialog.ShowAsync();
+                });
+            };
+
+            Telepat.GetInstance().CreateUserFailure += delegate (object s, string args) {
+                TaskHelper.RunOnUiThread(this.Dispatcher, () => {
+
+                    var dialog = new MessageDialog("Unable to create user: " + args);
+                    var task = dialog.ShowAsync();
+                });
+            };
+
+            Telepat.GetInstance().CreateUser("dorin@appscend.com", "parola", "Dorin");
+        }
+
+        private void login_Click(object sender, RoutedEventArgs e)
+        {
+            Telepat.GetInstance().LoginWithUsername("dorin@appscend.com", "parola");
+        }
+
+        private void logout_Click(object sender, RoutedEventArgs e)
         {
             Telepat.GetInstance().Logout();
         }
 
-        private async void button3_Click(object sender, RoutedEventArgs e)
-        {
-            var mChannel = await Telepat.GetInstance().Subscribe<answers>(Telepat
-                                                                          .GetInstance()
-                                                                          .GetContexts()
-                                                                          .Get("9a646cc5-d27c-4545-8007-ebcea82edc47"),
-                                                                          "answers");
-        }
-
-        private async void button4_Click(object sender, RoutedEventArgs e)
+        private async void contexts_Click(object sender, RoutedEventArgs e)
         {
         }
     }
